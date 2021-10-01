3b822e1b8e32706f07ed08e46bba32bd775fe00f
Ririkaze <77926438+Ririkaze@users.noreply.github.com>
2021-10-01 14:30:46 +0700

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+var modal = document.getElementById("modal");
+var btn = document.getElementById("btn");
+var closer = document.getElementById("btg");
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+window.onclick = function(event) {
+    if(event.target == closer) {
+        modal.style.display = "none";
+    }
+=======
 var modal = document.getElementsByClassName('calendar');
 var btn = document.getElementsByClassName("btn");
 
@@ -9,4 +22,5 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none"
     }
+>>>>>>> 3f8b29efd5b1271bee08d546191cd7105e3ed655
 }
